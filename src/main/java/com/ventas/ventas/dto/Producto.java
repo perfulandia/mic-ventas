2ee08bddb75a4ca832c05a419750ee225086e466
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-23 11:28:36 -0400

    arreglar ruta package

--- src/main/java/com/ventas/ventas/dto/Producto.java
+++ src/main/java/com/ventas/ventas/dto/Producto.java
@@ -1,11 +1,9 @@
-package com.ventas.ventas.model;
+package com.ventas.ventas.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 
 public class Producto {
