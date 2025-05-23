f2f8acd515f44d6029329d2642c5075d5000c444
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-23 11:53:34 -0400

    inicio implementacion consumir api inv

--- src/main/java/com/ventas/ventas/service/FacturaService.java
+++ src/main/java/com/ventas/ventas/service/FacturaService.java
@@ -3,8 +3,11 @@ package com.ventas.ventas.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.ventas.ventas.dto.ProductoDto;
 import com.ventas.ventas.model.Factura;
 import com.ventas.ventas.repository.FacturaRepository;
 
@@ -17,7 +20,18 @@ public class FacturaService {
     @Autowired
     private FacturaRepository facturaRepository;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     public List<Factura> findAll(){
         return facturaRepository.findAll();
+    }
+
+    public ProductoDto getProductos(List<Long> ids){
+        String API_URL = "http://localhost:8080/api/v1/inventario";
+        ResponseEntity<ProductoDto> response = restTemplate.getForEntity(API_URL, ProductoDto.class);
+
+        ProductoDto producto = response.getBody();
+        return producto;
     }
 }
