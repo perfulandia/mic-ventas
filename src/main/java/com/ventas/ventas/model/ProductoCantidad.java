f1618094690732f8836acc609c6e64a559a64034
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-05-26 20:30:36 -0400

    agregar constructor vacio

--- src/main/java/com/ventas/ventas/model/ProductoCantidad.java
+++ src/main/java/com/ventas/ventas/model/ProductoCantidad.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Embeddable
 public class ProductoCantidad {
 
